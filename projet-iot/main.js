ec1021ddbec1c0af18408234bcdfec0119ec774f
git <loislegentilnoclain@gmail.com>
2026-04-02 17:15:40 +0200

    init electron

---
+++ projet-iot/main.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 375,
+  });
+
+  win.loadFile('main.html');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
