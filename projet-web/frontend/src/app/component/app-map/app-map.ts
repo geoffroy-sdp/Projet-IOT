dc5a770cd2aa10a7b3aeab5785d89f6c0ab891f9
alexis <alexis.tourville@gmail.com>
2026-04-16 16:00:44 +0200

    push tout ca

--- projet-web/frontend/src/app/component/app-map/app-map.ts
+++ projet-web/frontend/src/app/component/app-map/app-map.ts
@@ -1,4 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as L from 'leaflet';
+
+// Fix des icônes Leaflet avec Angular/Webpack
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-app-map',
@@ -6,6 +23,22 @@ import { Component } from '@angular/core';
   templateUrl: './app-map.html',
   styleUrl: './app-map.css',
 })
-export class AppMap {
+export class AppMap implements AfterViewInit {
+  private map: any;
+
+  ngAfterViewInit(): void {
+  setTimeout(() => {
+    this.initMap();
+    this.map.invalidateSize();
+  }, 0);
+} 
+
+  private initMap(): void {
+    this.map = L.map('map').setView([44.354128, 1.203491], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(this.map);
+  }
 
 }
